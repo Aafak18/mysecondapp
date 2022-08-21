afb5ecba6f885cc937ccfc8388b56aea05170977
Afaq Hussain <hussainafaq18@gmail.com>
2022-08-21 19:53:05 +0500

    updated app.js

---
+++ src/Child.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Child = (props) => {
+
+    return(
+        <div>
+            <h2>This is first child</h2>
+            <h3>This is: {props.name}</h3>
+        </div>
+    );
+}
+
+export default Child;
